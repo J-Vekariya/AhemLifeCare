545e38f611bb046368d9642593f23431d51cd3da
Jayesh Vekariya <jayeshvekariya@Jayeshs-MacBook-Air.local>
2017-10-08 23:29:15 +0530

    Home Page design started

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use('/dist',express.static('/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
