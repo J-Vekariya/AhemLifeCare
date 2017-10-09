941e2262e916965d0a9cb8ac0981d9df65bc1a73
Jayesh Vekariya <jayeshvekariya@Jayeshs-MacBook-Air.local>
2017-10-09 20:32:47 +0530

    Home Page design started

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 // app.get('/dist',function(req,res){
 
 // })
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
 })
