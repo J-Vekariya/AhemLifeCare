639b5e0f327c61bffd76d580010eeb5e8498ce23
Jayesh Vekariya <jayeshvekariya@Jayeshs-MacBook-Air.local>
2017-10-09 20:31:07 +0530

    Home Page design started

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('dist',express.static(path.join(__dirname, 'dist')));
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+// app.get('/dist',function(req,res){
 
-app.listen(process.env.PORT, function () {
+// })
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
