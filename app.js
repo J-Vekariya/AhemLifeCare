cf2c90c77b63a48da1438976a7a9b7d07d74a065
Jayesh Vekariya <jayeshvekariya@Jayeshs-MacBook-Air.local>
2017-10-29 10:58:28 +0530

    Menu and Manufacturing page changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-  // res.sendFile(path.join(__dirname,'/dist/index.html'));
-})
+app.use('/',express.static(path.join(__dirname, 'dist')));
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+//   // res.sendFile(path.join(__dirname,'/dist/index.html'));
+// })
 // app.get('/dist',function(req,res){
 
 // })
