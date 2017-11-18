85ac985e98808959d0994496a46a4bb59ced3043
Jayesh Vekariya <jayeshvekariya@Jayeshs-MacBook-Air.local>
2017-11-18 13:18:41 +0530

    Node Deploy changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/',express.static(path.join(__dirname, 'dist')));
 // })
 app.use('*',function(req,res){
   console.log(req);
-  res.redirect('/');
+  res.sendFile(path.join(__dirname,'dist/index.html'));
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
